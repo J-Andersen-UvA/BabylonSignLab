2b3a0f2f4159ae80f06124d7c25464b44d66103d
Jari Andersen <j.andersen@uva.nl>
2024-04-04 16:43:37 +0200

    testing anim loading tactics on new branch

--- LoadingAnimation/AnimationLoader.js
+++ LoadingAnimation/AnimationLoader.js
@@ -1,33 +1,65 @@
-// Import animations for BabylonJS
-var getAnims = function (scene, mesh) {
+var getAnims = function(scene, mesh) {
     if (!scene) {
         console.error("Scene is undefined. Unable to import animations.");
         return;
     }
 
-    // const baseUrl = "https://thomlucc.github.io/Assets/AvatarDemo/";
-    // const sillyDance = BABYLON.SceneLoader.ImportAnimations(baseUrl, "SillyDance.glb", scene, false, BABYLON.SceneLoaderAnimationGroupLoadingMode.Clean, null,
-    //     onSuccess=function (skeletons) {
-    //         console.log("Animations loaded");
-    //     },
-    //     onProgress=function () {
-    //         console.log("Loading animations...");
+    // BABYLON.SceneLoader.OnPluginActivatedObservable.add(function(loader) {
+    //     if (loader.name == "gltf" || loader.name == "glb") {
+    //         loader.animationStartMode = BABYLON.GLTFLoaderAnimationStartMode.NONE;
     //     }
-    // );
+    // });
 
-    // TODO: When clicking the button twice, the animation loads, otherwise it doesnt load
-    BABYLON.SceneLoader.OnPluginActivatedObservable.add(function (loader) {
-        if (loader.name == "gltf" || loader.name == "glb") {
-            loader.animationStartMode = BABYLON.GLTFLoaderAnimationStartMode.NONE;
+    // let animations = [];
+    // let animationGroup = new BABYLON.AnimationGroup("group1");
+
+    // const importPromise = new Promise((resolve, reject) => {
+    //     BABYLON.SceneLoader.ImportAnimationsAsync("http://localhost:8080/MeshesAndAnims/", "3.glb", scene, false, BABYLON.SceneLoaderAnimationGroupLoadingMode.Clean, null,
+    //         onSucces = function(animatables) {
+    //             console.log("Animations loaded successfully I think.");
+    //             animations = animatables;
+    //             if (animatables.length > 0) {
+    //                 resolve(animatables);
+    //             } else {
+    //                 reject("No animations found.");
+    //             }
+    //         },
+    //         onProgress = function() {
+    //             console.log("Loading animations...");
+    //         },
+    //         onError = function(error) {
+    //             console.error("Error loading animations: " + error);
+    //             reject(error);
+    //         }
+    //     );
+    // });
+
+    // importPromise.then((animatables) => {
+    //     console.log("Animations loaded, adding to anim group");
+    //     console.log(animatables[0]);
+    //     animationGroup.addTargetedAnimation(animatables[0], mesh);
+    // }).catch((error) => {
+    //     console.error(error);
+    // });
+    // BABYLON.SceneLoader.ImportMeshAsync("", "/3.glb", scene);
+    BABYLON.SceneLoader.ImportMesh(null, "http://localhost:8080/MeshesAndAnims/", "onzinverhaal.glb", scene, function (newMeshes, particleSystems, skeletons) {
+        console.log("Mesh loaded");
+        console.log(skeletons);
+        // Check if skeletons array is not empty
+        if (skeletons.length > 0) {
+            // Extract animations from the first skeleton
+            var animations = skeletons[0].getAnimations();
+            console.log(animations);
+        } else {
+            console.log("No animations found.");
         }
     });
-
-    BABYLON.SceneLoader.ImportAnimations("http://localhost:8080/MeshesAndAnims/", "3.glb", scene, false, BABYLON.SceneLoaderAnimationGroupLoadingMode.Sync, null,
-        onSuccess=function (skeletons) {
-            console.log("Animations loaded");
-        },
-        onProgress=function () {
-            console.log("Loading animations...");
-        }
-    );
-}
+    // .then(function (result) {
+    //     _engine.hideLoadingUI();
+    //     //
+    //     assignAnimations(result.animationGroups)
+    // })
+    // .catch(function (error) {
+    //     console.error(_TAG + error);
+    // }
+};
