b09065572512ef35b2a44677c554d012d91ffda8
J-Andersen-UvA <j.andersen@uva.nl>
2024-04-11 13:43:01 +0200

    local axis of sphere

--- LoadingAnimation/SceneAndMeshLoader.js
+++ LoadingAnimation/SceneAndMeshLoader.js
@@ -55,6 +55,37 @@ var createScene = async function (canvas) {
     camera.attachControl(canvas, true);
     camera.target = sphere;
 
+    // Display axis of the sphere
+    localAxes(5, sphere, scene);
 
     return [scene, engine, result];
 };
+
+
+//Local Axes
+function localAxes(size, mesh, scene) {
+    var pilot_local_axisX = BABYLON.Mesh.CreateLines("pilot_local_axisX", [ 
+    new BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0), 
+    new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+    ], scene);
+    pilot_local_axisX.color = new BABYLON.Color3(1, 0, 0);
+
+    pilot_local_axisY = BABYLON.Mesh.CreateLines("pilot_local_axisY", [
+        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
+        new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
+    ], scene);
+    pilot_local_axisY.color = new BABYLON.Color3(0, 1, 0);
+
+    var pilot_local_axisZ = BABYLON.Mesh.CreateLines("pilot_local_axisZ", [
+        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0 , -0.05 * size, size * 0.95),
+        new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0, 0.05 * size, size * 0.95)
+        ], scene);
+    pilot_local_axisZ.color = new BABYLON.Color3(0, 0, 1);
+
+    var local_origin = BABYLON.MeshBuilder.CreateBox("local_origin", {size:1}, scene);
+    local_origin.isVisible = false;
+
+    pilot_local_axisX.parent = mesh;
+    pilot_local_axisY.parent = mesh;
+    pilot_local_axisZ.parent = mesh; 
+}
