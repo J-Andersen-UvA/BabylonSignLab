588c5c786faea30af04f71b3fc6660bb8e918fc0
Gomer Otterspeer <gotterspeer@gmail.com>
2024-04-13 17:59:24 +0200

    we now can create sentences in NGT/NmG

--- LoadingAnimation/signCollectLoader.js
+++ LoadingAnimation/signCollectLoader.js
@@ -1,12 +1,14 @@
-async function signCollectLoader(thema) {
+async function signCollectLoader(thema, filter, limit) {
     try {
-        const response = await fetch('https://leffe.science.uva.nl:8043/fetch_all.php?limit=10&offset=0&handle=themaFilter&thema=' + thema);
+
+        const response = await fetch(`https://leffe.science.uva.nl:8043/fetch_all.php?limit=${limit}&offset=0&handle=${filter}&thema=` + thema);
+        
+        
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         const text = await response.text();
         const data = JSON.parse(text);
-        console.log(data)
         return data;
     } catch (error) {
         console.error("Failed to load data:", error);
