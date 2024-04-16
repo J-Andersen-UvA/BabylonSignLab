eae975d1046f3fabe3ce8dbc693a434df7fd1a57
J-Andersen-UvA <j.andersen@uva.nl>
2024-04-16 13:18:59 +0200

    Babylon has a weird way of dealing with imported meshes. This codes makes sure that we only have glassesguy in the scene and no bloat. It might not work in the future depending on how we integrate avatars, but we can always remove the cleanup lines.

--- LoadingAnimation/SceneAndMeshLoader.js
+++ LoadingAnimation/SceneAndMeshLoader.js
@@ -26,12 +26,16 @@ var createScene = async function (canvas) {
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
-    // const result = await BABYLON.SceneLoader.ImportMeshAsync(null, "http://localhost:8080/MeshesAndAnims/", "glassesGuyUEGLTF.gltf", scene);
-    // const result = await BABYLON.SceneLoader.ImportMeshAsync(null, "http://localhost:8080/MeshesAndAnims/", "glassesGuyFBXConverted.gltf", scene);
     const result = await BABYLON.SceneLoader.ImportMeshAsync(null, "http://localhost:8080/MeshesAndAnims/", "GlassesGuyBabylon.glb", scene);
-    // const result = await BABYLON.SceneLoader.ImportMeshAsync(null, "http://localhost:8080/MeshesAndAnims/", "UEfbxCOCOSgltfBABYLONglb.glb", scene);
+    var glassesGuy = result.meshes[0].getChildren()[0].getChildren()[0];
+    glassesGuy.parent = null;
+    result.meshes[0].dispose();
+    result.meshes[0] = null;
+    console.error(result);
+    console.error(scene);
 
-    var topLight = new BABYLON.PointLight("topLight", result.meshes[0].getAbsolutePosition().add(new BABYLON.Vector3(0, 4, 0)), scene);
+
+    var topLight = new BABYLON.PointLight("topLight", glassesGuy.getAbsolutePosition().add(new BABYLON.Vector3(0, 4, 0)), scene);
     topLight.diffuse = new BABYLON.Color3(1, 1, 1); // Set light color
     topLight.intensity = 1; // Set light intensity
     
