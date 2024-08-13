3e399616b6342c840a6c7bf8926532146d2d64fa
J-Andersen-UvA <j.andersen@uva.nl>
2024-08-13 10:23:45 +0200

    flask for testing docker implementation with ueretarget server

---
+++ LoadingAnimation/tmpFlask.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def serve_html():
+    return send_from_directory('.', 'index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
